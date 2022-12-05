e10c93b8939dd3ee961e3ae0b8c12d21a5ad843a
reannu123 <reannumon123@gmail.com>
2022-12-05 16:00:11 +0800

    Added placeholder home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import { Alert, Container, Form, Button } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { instance, auth } from "../api/axios";
-
+import '../main.css';
 function Home() {
   const [user, setUser] = useState({
     username: "",
@@ -31,22 +31,20 @@ function Home() {
   return (
     <>
       {(
-        <Container className="py-5 my-5 border rounded-3 d-flex align-items-center justify-content-center">
+        <div className="container py-5 my-5 border rounded-3 d-flex row justify-content-center m-auto">
           
-          <Form onSubmit={handleLogout}>
-          <h1 className="mb-5 text-center">HOME</h1> 
-          <Button variant="primary" type="submit">
-              Log Out
-            </Button>
-            <Button variant="primary" onClick={()=>{navigate('/login')}}>
-              Log In
-            </Button>
-            <Button variant="primary" onClick={()=>{navigate('/register')}}>
-              Register
-            </Button>
-            </Form>
-          
-        </Container>
+          <div class="p-5">
+            <h1 className="display-5 mb-5 fw-bold text-center">TrendTracker</h1> 
+            <p className="lead text-center">Welcome to TrendTracker, an AI-powered Product demand forecasting tool. </p>
+          </div>
+          <div class="px-5">
+            <hr/>
+          </div>
+          <div className="p-5 input-group w-75 column align-items-center">
+            <input type="search" placeholder="Search for a category" className="form-control" />
+            <button type="button" class="btn btn-outline-primary">SEARCH</button>
+          </div>
+        </div>
       )}
     </>
   );
