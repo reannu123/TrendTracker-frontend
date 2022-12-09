f17a98b8da8011c15ac6a83c12bb530cdd027ddb
reannu123 <reannumon123@gmail.com>
2022-12-09 15:10:50 +0800

    Added prototype rankings component

---
+++ src/components/ranking.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+function Ranking() {
+  const items = [
+    {
+      name: "Item 1",
+      description: "This is the first item",
+    },
+    {
+      name: "Item 2",
+      description: "This is the second item",
+    },
+    {
+      name: "Item 3",
+      description: "This is the third item",
+    },
+    {
+      name: "Item 4",
+      description: "This is the fourth item",
+    },
+  ];
+  return (
+    <>
+      <div className="container mb-5">
+        <div className="row">
+          <div className="col-12 my-3 text-center">
+            <h1>Current Rankings</h1>
+          </div>
+          {items.map((item, index) => (
+            <div className="col-12 mb-1" key={index}>
+              <div className="col-12 d-flex col justify-content-center">
+                <div className=" rounded-2 w-25 text-center d-flex justify-content-center align-items-center">
+                  <h5>{index + 1}</h5>
+                </div>
+                <div className="card w-50">
+                  <div className="card-body justify-content-center">
+                    <h5 className="card-title">{item.name}</h5>
+                    <p className="card-text">{item.description}</p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Ranking;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,8 @@ import { Alert, Container, Form, Button } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { instance, auth } from "../api/axios";
-import '../main.css';
+import "../main.css";
+import Ranking from "../components/ranking";
 function Home() {
   const [user, setUser] = useState({
     username: "",
@@ -10,7 +11,7 @@ function Home() {
   });
 
   const [success, setSuccess] = useState(false);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
@@ -20,7 +21,7 @@ function Home() {
     e.preventDefault();
     try {
       auth.delete("/logout");
-      console.log("Logged Out")
+      console.log("Logged Out");
       localStorage.removeItem("accessToken");
       setSuccess(true);
     } catch (e) {
@@ -30,22 +31,22 @@ function Home() {
 
   return (
     <>
-      {(
-        <div className="container py-5 my-5 border rounded-3 d-flex row justify-content-center m-auto">
-          
-          <div className="p-5">
-            <h1 className="display-5 mb-5 fw-bold text-center">TrendTracker</h1> 
-            <p className="lead text-center">Welcome to TrendTracker, an AI-powered Product demand forecasting tool. </p>
+      {
+        <div className="container my-3 d-flex row justify-content-center m-auto">
+          <div className="pt-5">
+            <h1 className="display-5 mb-3 fw-bold text-center">TrendTracker</h1>
+            <p className="lead text-center">
+              Welcome to TrendTracker, an AI-powered Product demand forecasting
+              tool.{" "}
+            </p>
           </div>
           <div className="px-5">
-            <hr/>
+            <hr />
           </div>
-          <div className="p-5 input-group w-75 column align-items-center">
-            <input type="search" placeholder="Search for a category" className="form-control" />
-            <button type="button" className="btn btn-outline-primary">SEARCH</button>
-          </div>
+
+          <Ranking />
         </div>
-      )}
+      }
     </>
   );
 }
