91d8165856b16d84106dbb11704c7ef30a6ce18a
reannu123 <reannumon123@gmail.com>
2022-12-12 12:30:34 +0000

    Added simple graphs for each product and user

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,11 +1,78 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { LineChart, DoughnutChart } from "../components/Charts";
+import { faker } from "@faker-js/faker";
 
 function Product() {
   const { id } = useParams();
+  const doughnutData = {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
+  const lineData = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: labels.map(() =>
+          faker.datatype.number({ min: -1000, max: 1000 })
+        ),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Dataset 2",
+        data: labels.map(() =>
+          faker.datatype.number({ min: -1000, max: 1000 })
+        ),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+
   return (
-    <div className="container">
-      <h1 className="card m-5 pb-5 border-bottom">Product {id}</h1>
+    <div className="container ">
+      <h1 className="card m-5 pb-5">Product {id}</h1>
+      <div className="container d-flex justify-content-around m-auto align-items-center">
+        <div className="card ">
+          <LineChart data={lineData} />
+        </div>
+        <div className="card">
+          <DoughnutChart data={doughnutData} />
+        </div>
+      </div>
     </div>
   );
 }
